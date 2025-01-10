3d49b4baf5e95e0b0c19ec153292583219cb49b7
Byungwook-Kim <bwkim@cubrid.com>
2025-01-10 20:52:36 +0900

    precision, scale to qfile_update_domains_on_type_list

--- src/query/list_file.c
+++ src/query/list_file.c
@@ -6237,7 +6237,13 @@ qfile_update_domains_on_type_list (THREAD_ENTRY * thread_p, QFILE_LIST_ID * list
 	  goto exit_on_error;
 	}
 
-      if (TP_DOMAIN_TYPE (list_id_p->type_list.domp[count]) == DB_TYPE_VARIABLE)
+      /* To prevent decimal points from being truncated */
+      if (TP_DOMAIN_TYPE (list_id_p->type_list.domp[count]) == DB_TYPE_NUMERIC)
+	{
+	  list_id_p->type_list.domp[count]->precision = reg_var_p->value.domain->precision;
+	  list_id_p->type_list.domp[count]->scale = reg_var_p->value.domain->scale;
+	}
+      else if (TP_DOMAIN_TYPE (list_id_p->type_list.domp[count]) == DB_TYPE_VARIABLE)
 	{
 	  if (TP_DOMAIN_TYPE (reg_var_p->value.domain) == DB_TYPE_VARIABLE)
 	    {
